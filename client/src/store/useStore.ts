868bb0bad90f7b80ea368467f0c3d8b9b6481087
Ivan Križaj <ivan.krizaj321@gmail.com>
2025-07-02 18:46:24 +0200

    fix login page showing on refresh site

--- client/src/store/useStore.ts
+++ client/src/store/useStore.ts
@@ -24,7 +24,7 @@ export const useStore = create<AppState>()(
             user: null,
             currentUser: null,
             isAuthenticated: false,
-            isLoading: false,
+            isLoading: true,
             error: null,
 
             // Auth actions
